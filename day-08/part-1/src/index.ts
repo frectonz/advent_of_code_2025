3442a3a95862adf17fd12631cc99f9a946a5422e
frectonz <fraol0912@gmail.com>
2025-12-08 16:19:34 +0300

    feat: implement day 08 part 01

--- day-08/part-1/src/index.ts
+++ day-08/part-1/src/index.ts
@@ -1,121 +1,88 @@
-function calcDistance(
-	x1: number,
-	y1: number,
-	z1: number,
-	x2: number,
-	y2: number,
-	z2: number,
-): number {
-	const dx = x2 - x1;
-	const dy = y2 - y1;
-	const dz = z2 - z1;
+class UnionFind {
+	par: number[];
+	size: number[];
+
+	constructor(n: number) {
+		this.par = Array.from({ length: n }, (_, i) => i);
+		this.size = Array(n).fill(1);
+	}
+
+	find(x: number): number {
+		while (this.par[x] !== x) {
+			this.par[x] = this.par[this.par[x]!]!;
+			x = this.par[x]!;
+		}
+		return x;
+	}
+
+	union(x: number, y: number): boolean {
+		const parX = this.find(x);
+		const parY = this.find(y);
+
+		if (parX === parY) return false;
+
+		if (this.size[parX]! >= this.size[parY]!) {
+			this.par[parY] = parX;
+			this.size[parX]! += this.size[parY]!;
+		} else {
+			this.par[parX] = parY;
+			this.size[parY]! += this.size[parX]!;
+		}
+
+		return true;
+	}
+}
+
+type Pos = { x: number; y: number; z: number };
+
+function getDist(first: Pos, second: Pos): number {
+	const { x: x1, y: y1, z: z1 } = first;
+	const { x: x2, y: y2, z: z2 } = second;
+
+	const dx = x1 - x2;
+	const dy = y1 - y2;
+	const dz = z1 - z2;
+
 	return dx * dx + dy * dy + dz * dz;
 }
 
 async function main() {
-	const file = Bun.file("inputs/example.txt");
-	const input = (await file.text()).trim();
+	const text = await Bun.file("inputs/input.txt").text();
+	const lines = text.trim().split(/\r?\n/).filter(Boolean);
 
-	const positions = input
-		.split("\n")
-		.map((x) => x.split(",").map((x) => parseInt(x)));
+	const coordinates: Pos[] = lines.map((line) => {
+		const [x, y, z] = line
+			.trim()
+			.split(",")
+			.map((v) => parseInt(v, 10));
+		return { x: x!, y: y!, z: z! };
+	});
 
-	const circuits: Record<string, [number, number, number][]> = {};
+	const n = coordinates.length;
+	const distances: [number, number, number][] = [];
 
-	for (const [x, y, z] of positions) {
-		const key = `${x}:${y}:${z}`;
-		circuits[key] = [[x!, y!, z!]];
-	}
-
-	let step = 0;
-	const circuitKeys = Object.keys(circuits);
-
-	while (step < 10) {
-		console.log("connection", step);
-
-		let minDistance = Infinity;
-		let minJunction:
-			| [[number, number, number], [number, number, number]]
-			| null = null;
-
-		for (const key1 of circuitKeys) {
-			const [x1, y1, z1] = circuits[key1]![0]!;
-			const values = circuits[key1]!;
-
-			for (const key2 of circuitKeys) {
-				const [x2, y2, z2] = circuits[key2]![0]!;
-
-				if (x1 == x2 && y1 == y2 && z1 == z2) continue;
-				if (values.find(([x, y, z]) => x == x2 && y == y2 && z == z2)) continue;
-
-				const distance = calcDistance(x1!, y1!, z1!, x2!, y2!, z2!);
-
-				if (distance < minDistance) {
-					minDistance = distance;
-					minJunction = [
-						[x1!, y1!, z1!],
-						[x2!, y2!, z2!],
-					];
-				}
-			}
-		}
-
-		const [[x1, y1, z1], [x2, y2, z2]] = minJunction!;
-
-		const key1 = `${x1}:${y1}:${z1}`;
-		const key2 = `${x2}:${y2}:${z2}`;
-
-		const values1 = circuits[key1]!;
-		values1.push([x2, y2, z2]);
-		for (const val of values1) {
-			const values = circuits[val.join(":")]!;
-
-			const contains = values.find(
-				([x, y, z]) => x == x2 && y == y2 && z == z2,
-			);
-
-			if (!contains) values.push([x2, y2, z2]);
-		}
-
-		const values2 = circuits[key2]!;
-		values2.push([x1, y1, z1]);
-		for (const val of values2) {
-			const values = circuits[val.join(":")]!;
-
-			const contains = values.find(
-				([x, y, z]) => x == x1 && y == y1 && z == z1,
-			);
-
-			if (!contains) values.push([x1, y1, z1]);
-		}
-
-		step++;
-	}
-
-	for (const pos of positions) {
-		const allCircuits = Object.entries(circuits);
-
-		const [x, y, z] = pos;
-
-		const containedIn = allCircuits
-			.filter(([_, circuit]) =>
-				circuit.find(([x1, y1, z1]) => x == x1 && y == y1 && z == z1),
-			)
-			.toSorted(([_, a], [__, b]) => a.length - b.length)
-			.map(([key, _]) => key);
-
-		delete containedIn[containedIn.length - 1];
-
-		for (const node of containedIn) {
-			delete circuits[node];
+	for (let i = 0; i < n; i++) {
+		for (let j = i + 1; j < n; j++) {
+			const currDist = getDist(coordinates[i]!, coordinates[j]!);
+			distances.push([currDist, i, j]);
 		}
 	}
 
-	const counts = Object.values(circuits)
-		.map((x) => x.length)
-		.toSorted((a, b) => b - a);
-	const result = counts[0]! * counts[1]! * counts[2]!;
-	console.log(result);
+	const uf = new UnionFind(n);
+	distances.sort((a, b) => a[0] - b[0]);
+
+	const limit = 1000;
+	const len = Math.min(distances.length, limit);
+
+	for (let i = 0; i < len; i++) {
+		const [, firstInd, secondInd] = distances[i]!;
+		uf.union(firstInd, secondInd);
+	}
+
+	const sortedComp = [...uf.size].sort((a, b) => b - a);
+	const [x, y, z] = sortedComp;
+
+	console.log(x! * y! * z!);
 }
 
 main();
