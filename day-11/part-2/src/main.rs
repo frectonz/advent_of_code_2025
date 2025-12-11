62d073dc4e12e9858b14161e2560f33d329c6b74
frectonz <fraol0912@gmail.com>
2025-12-12 00:06:24 +0300

    feat: implement day 11 part 02 with `pathfinding`

--- day-11/part-2/src/main.rs
+++ day-11/part-2/src/main.rs
@@ -1,61 +1,33 @@
-use std::{
-    collections::{HashMap, HashSet},
-    hash::RandomState,
-};
+use std::collections::HashMap;
 
-use petgraph::{
-    algo::all_simple_paths,
-    graph::{DiGraph, NodeIndex},
-};
+use pathfinding::prelude::count_paths;
 
 fn main() {
-    let input = include_str!("../inputs/example.txt");
+    let input = include_str!("../inputs/input.txt");
 
-    let connections = input
+    let mut connections = input
         .lines()
         .flat_map(|line| {
             let (from, tos) = line.split_once(':')?;
             let tos = tos.trim().split_whitespace().collect::<Vec<_>>();
             Some((from.trim(), tos))
         })
-        .collect::<Vec<_>>();
+        .collect::<HashMap<_, _>>();
+    connections.insert("out", vec![]);
 
-    let edges = connections
-        .into_iter()
-        .map(|(from, tos)| tos.into_iter().map(|to| (from.to_owned(), to.to_owned())))
-        .flatten()
-        .collect::<Vec<_>>();
+    let count = count_paths(
+        ("svr", false, false),
+        |&(name, fft, dac)| {
+            connections[name].iter().map(move |&next_label| {
+                (
+                    next_label,
+                    fft || next_label == "fft",
+                    dac || next_label == "dac",
+                )
+            })
+        },
+        |&c| c == ("out", true, true),
+    );
 
-    let unique_nodes = edges
-        .clone()
-        .into_iter()
-        .map(|(from, to)| vec![from, to].into_iter())
-        .flatten()
-        .collect::<HashSet<_>>();
-
-    let mut map: HashMap<String, NodeIndex> = HashMap::new();
-
-    let mut graph = DiGraph::<String, ()>::new();
-
-    for node in unique_nodes {
-        let idx = graph.add_node(node.to_owned());
-        map.insert(node, idx);
-    }
-
-    for (from, to) in edges {
-        let from = map[&from];
-        let to = map[&to];
-
-        graph.add_edge(from, to, ());
-    }
-
-    let paths = all_simple_paths::<Vec<_>, _, RandomState>(&graph, map["svr"], map["out"], 0, None)
-        .collect::<Vec<_>>();
-
-    let valid_paths = paths
-        .into_iter()
-        .filter(|path| path.contains(&map["fft"]) && path.contains(&map["dac"]))
-        .count();
-
-    dbg!(valid_paths);
+    dbg!(count);
 }
